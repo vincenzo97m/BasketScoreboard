879fa5204f763509c5d88bf6c5cab1f259e3574b
Vincenzo Morabito <vincenzomorabito97@gmail.com>
2024-12-08 16:45:55 +0100

    Implementata la funzione di start e stop timer premendo la barra spaziatrice.

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def set_fullscreen_on_secondary_screen():
     else:
         print("\n\n!!!!!Non ci sono schermi secondari connessi.!!!!!")
 
-# Funzione per aprire la finestra controller sul monitor secondario
+# Funzione per aprire la finestra CONTROLLER sul monitor secondario
 def set_controller_on_secondary_screen():
     monitors = get_monitors()  # Ottieni tutte le informazioni sugli schermi connessi
     if len(monitors) > 1:
@@ -42,30 +42,27 @@ def set_controller_on_secondary_screen():
 
 # Funzioni per gestire il tabellone
 def update_score(label, increment, display_label=None):
-    """Aggiorna il punteggio di una squadra e, se fornito, aggiorna anche l'etichetta nel controller."""
+    """Aggiorna il punteggio di una squadra e, se fornito, aggiorna anche l'etichetta nel CONTROLLER."""
     current = int(label["text"])
     new_score = max(0, current + increment)
     label.config(text=str(new_score))
-    if display_label:
-        display_label.config(text=str(new_score))  # Aggiorna l'etichetta nel controller
+    display_label.config(text=str(new_score))  # Aggiorna l'etichetta nel CONTROLLER
 
 def update_period(label, increment, period_display_label=None):
-    """Aggiorna il periodo del gioco e, se fornito, aggiorna anche l'etichetta nel controller."""
+    """Aggiorna il periodo del gioco e, se fornito, aggiorna anche l'etichetta nel CONTROLLER."""
     period_values = ["1QT", "2QT", "3QT", "4QT", "1OT", "2OT", "3OT", "4OT", "5OT", "6OT", "7OT", "8OT", "9OT"]
     try:
         current_index = period_values.index(label["text"])
         new_index = (current_index + increment) % len(period_values)
         label.config(text=period_values[new_index])
-        if period_display_label:
-            period_display_label.config(text=period_values[new_index])  # Aggiorna l'etichetta nel controller
+        period_display_label.config(text=period_values[new_index])  # Aggiorna l'etichetta nel CONTROLLER
     except ValueError:
         label.config(text="1QT")
 
 def update_time(label, minutes, seconds, time_display_label=None):
-    """Aggiorna il tempo visualizzato e, se fornito, aggiorna anche l'etichetta nel controller."""
+    """Aggiorna il tempo visualizzato e, se fornito, aggiorna anche l'etichetta nel CONTROLLER."""
     label.config(text=f"{minutes}:{seconds:02d}")
-    if time_display_label:
-        time_display_label.config(text=f"{minutes}:{seconds:02d}")  # Aggiorna l'etichetta nel controller
+    time_display_label.config(text=f"{minutes}:{seconds:02d}")  # Aggiorna l'etichetta nel CONTROLLER
 
 def set_time():
     """Imposta il tempo iniziale."""
@@ -119,7 +116,7 @@ def change_team_name(team, parent):
         (locali_label if team == "HOME" else ospiti_label).config(text=new_name.upper())
 
 def on_close_controller():
-    """Gestisce la chiusura della finestra del controller."""
+    """Gestisce la chiusura della finestra del CONTROLLER."""
     if messagebox.askquestion("Conferma Chiusura", "Sei sicuro di voler chiudere Tabellone Segnapunti?", parent=controller) == 'yes':
         tabellone.quit()
         controller.quit()
@@ -148,7 +145,7 @@ locali_label.grid(row=1, column=0, pady=5)
 locali_score = tk.Label(tabellone, text="0", bg="black", fg="green", font=score_font)
 locali_score.grid(row=2, column=0)
 
-ospiti_label = tk.Label(tabellone, text="VISITOR", bg="black", fg="white", font=small_font)
+ospiti_label = tk.Label(tabellone, text="GUEST", bg="black", fg="white", font=small_font)
 ospiti_label.grid(row=1, column=2, pady=5)
 ospiti_score = tk.Label(tabellone, text="0", bg="black", fg="green", font=score_font)
 ospiti_score.grid(row=2, column=2)
@@ -160,6 +157,12 @@ period_value.grid(row=1, column=1, padx=20)
 time_label = tk.Label(tabellone, text="10:00", bg="black", fg="red", font=time_font)
 time_label.grid(row=0, columnspan=3, stick="nsew")
 
+# Funzione per gestire la cattura della barra spaziatrice
+def handle_spacebar(event):
+    """Gestisce la pressione della barra spaziatrice per avviare o fermare il timer."""
+    start_stop_timer()
+    return "break"
+
 # Creazione del CONTROLLER
 controller = tk.Tk()
 controller.title("Controller - Segnapunti")
@@ -167,33 +170,34 @@ controller.configure(bg="black")
 controller.grid_anchor("center")
 controller.protocol("WM_DELETE_WINDOW", on_close_controller)
 
-# Posiziona la finestra controller sul monitor secondario
+# Posiziona la finestra CONTROLLER sul monitor secondario
 set_controller_on_secondary_screen()
 
-button_frame = tk.Frame(controller, bg="blue")
-button_frame.grid(row=0, column=0, padx=20, pady=20)
+# Comando per bind barra sbaziatrice <---> Start/Stop Timer
+controller.bind("<space>", handle_spacebar)
 
-# Etichette per visualizzare i punteggi nel controller
+button_frame = tk.Frame(controller, bg="blue")
+
+# Etichette per visualizzare i punteggi nel CONTROLLER
 locali_score_display = tk.Label(controller, text="0", bg="black", fg="green", font=("Arial", 100))
 locali_score_display.grid(row=1, column=0, padx=40)
 ospiti_score_display = tk.Label(controller, text="0", bg="black", fg="green", font=("Arial", 100))
 ospiti_score_display.grid(row=1, column=2, padx=40)
 
-# Etichetta per visualizzare il periodo nel controller
+# Etichetta per visualizzare il periodo nel CONTROLLER
 period_value_display = tk.Label(controller, text="1QT", bg="black", fg="red", font=("Arial", 70))
 period_value_display.grid(row=1, column=1)
 
-# Etichetta per visualizzare il tempo nel controller
+# Etichetta per visualizzare il tempo nel CONTROLLER
 time_label_display = tk.Label(controller, text="10:00", bg="black", fg="red", font=("Arial", 100))
 time_label_display.grid(row=4, column=1)
 
-# Pulsanti e funzioni per il controller
-
+# Pulsanti e funzioni per il CONTROLLER
 tk.Button(controller, text="+HOME", font=("Arial", 70), bg="white", command=lambda: update_score(locali_score, 1, locali_score_display)).grid(row=0, column=0)
 tk.Button(controller, text="-HOME", font=("Arial", 70), bg="white", command=lambda: update_score(locali_score, -1, locali_score_display)).grid(row=2, column=0)
 
-tk.Button(controller, text="+VISITOR", font=("Arial", 70), bg="white", command=lambda: update_score(ospiti_score, 1, ospiti_score_display)).grid(row=0, column=2)
-tk.Button(controller, text="-VISITOR", font=("Arial", 70), bg="white", command=lambda: update_score(ospiti_score, -1, ospiti_score_display)).grid(row=2, column=2)
+tk.Button(controller, text="+GUEST", font=("Arial", 70), bg="white", command=lambda: update_score(ospiti_score, 1, ospiti_score_display)).grid(row=0, column=2)
+tk.Button(controller, text="-GUEST", font=("Arial", 70), bg="white", command=lambda: update_score(ospiti_score, -1, ospiti_score_display)).grid(row=2, column=2)
 
 tk.Button(controller, text="+ Period", font=("Arial", 50), bg="white", command=lambda: update_period(period_value, 1, period_value_display)).grid(row=0, column=1)
 tk.Button(controller, text="- Period", font=("Arial", 50), bg="white", command=lambda: update_period(period_value, -1, period_value_display)).grid(row=2, column=1)
@@ -204,7 +208,7 @@ start_stop_button.grid(row=3, column=1)
 tk.Button(controller, text="Reset Timer", font=("Arial", 40), bg="white", command=reset_timer).grid(row=3, column=2)
 
 tk.Button(controller, text="Cambia Nome HOME", font=("Arial", 30), bg="white", command=lambda: change_team_name("HOME", controller)).grid(row=4, column=0, pady=10)
-tk.Button(controller, text="Cambia Nome VISITOR", font=("Arial", 30), bg="white", command=lambda: change_team_name("VISITOR", controller)).grid(row=4, column=2, pady=10)
+tk.Button(controller, text="Cambia Nome GUEST", font=("Arial", 30), bg="white", command=lambda: change_team_name("GUEST", controller)).grid(row=4, column=2, pady=10)
 
 # Mostra il messaggio informativo con le opzioni Sì e No
 response = messagebox.askyesno(
@@ -217,7 +221,7 @@ response = messagebox.askyesno(
         "3. Spunta \"Imposta come schermo principale\".\n\n"
         "Lo schermo collegato è stato già impostato come principale?"
     ),
-    parent=controller  # Associa il messaggio alla finestra del controller
+    parent=controller  # Associa il messaggio alla finestra del CONTROLLER
 )
 
 # Se l'utente risponde "No", chiudi l'applicazione
